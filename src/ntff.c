dee275a37f95984d125baa73daf0f844394f018b
Valentine Lilova <elventian@gmail.com>
2020-03-10 00:54:22 +0300

    custom es_out_t for inner demux

--- src/ntff.c
+++ src/ntff.c
@@ -15,9 +15,11 @@
 #include <vlc_input_item.h>
 #include <vlc_dialog.h>
 #include <vlc_demux.h>
+#include <vlc_block.h>
 #include <vlc_modules.h>
 #include <vlc_stream_extractor.h>
- 
+
+
 static int Open(vlc_object_t *);
 static void Close(vlc_object_t *);
  
@@ -113,22 +115,98 @@ typedef struct
 struct demux_sys_t
 {
 	demux_t *fdemux;
-	stream_t *stream;
+	struct es_out_t es;
+	stream_t *stream;	
+	
 	time_interval scenes[3];
 	int scenes_num;
 	int cur_scene;
+	bool need_skip_scene;
 };
+
+struct es_out_sys_t
+{
+	struct es_out_t *out;
+	demux_t *p_demux;
+};
+
+#include <vlc_es.h>
+static es_out_id_t *ntff_es_add(es_out_t *ntff_out, const es_format_t *format)
+{
+	struct es_out_t *out = ntff_out->p_sys->out;
+	es_out_id_t *res = out->pf_add(out, format);
+	
+	char *fstr = (format->i_cat == VIDEO_ES) ? "video" : (format->i_cat == AUDIO_ES) ? "audio" : "unknown";
+	msg_Dbg( ntff_out->p_sys->p_demux, "~~~~~~~~~~ntff_es_add %s = %i", fstr, *((int *)res));
+	return res;
+}
+
+static int ntff_es_send(es_out_t *ntff_out, es_out_id_t *id, block_t *block)
+{
+	msg_Dbg( ntff_out->p_sys->p_demux, "~~~~~~~~~~ntff_es_send (%i): i_pts = %li, i_dts = %li", 
+		*((int *)id), block->i_pts, block->i_dts);
+	
+	
+	int64_t cur_time = block->i_pts;
+	if (cur_time == 0)
+	{
+		cur_time = block->i_dts;
+	}
+	
+	demux_sys_t *d_sys = ntff_out->p_sys->p_demux->p_sys;
+	if (cur_time > d_sys->scenes[d_sys->cur_scene].end)
+	{
+		d_sys->need_skip_scene = true;
+		return VLC_SUCCESS;
+	}
+	
+	struct es_out_t *out = ntff_out->p_sys->out;
+	return out->pf_send(out, id, block);
+}
+
+static int ntff_es_control(es_out_t *ntff_out, int i_query, va_list va)
+{
+	struct es_out_t *out = ntff_out->p_sys->out;
+	return out->pf_control(out, i_query, va);
+}
+
+static void ntff_es_del(es_out_t *ntff_out, es_out_id_t *id)
+{
+	struct es_out_t *out = ntff_out->p_sys->out;
+	out->pf_del(out, id);
+}
+
+static void ntff_es_destroy(es_out_t *ntff_out)
+{
+	struct es_out_t *out = ntff_out->p_sys->out;
+	out->pf_destroy(out);
+}
+
+static void ntff_register_es(demux_t *p_demux, struct es_out_t *es)
+{
+	es->pf_add = ntff_es_add;
+	es->pf_send = ntff_es_send;
+	es->pf_del = ntff_es_del;
+	es->pf_control = ntff_es_control;
+	es->pf_destroy = ntff_es_destroy;
+	
+	es->p_sys = calloc( 1, sizeof( es_out_sys_t ) );
+	es->p_sys->out = p_demux->out;
+	es->p_sys->p_demux = p_demux;
+	
+}
 
 static int Demux( demux_t * p_demux )
 {
     //msg_Dbg( p_demux, "~~~~NTFF Demux called");
 	
 	int64_t cur_time, pts_delay;
-	demux_Control(p_demux->p_sys->fdemux, DEMUX_GET_TIME, &cur_time);
-	demux_Control(p_demux->p_sys->fdemux, DEMUX_GET_PTS_DELAY, &pts_delay);
-	time_interval *cur_scene = &p_demux->p_sys->scenes[p_demux->p_sys->cur_scene];
+	demux_sys_t * p_sys = p_demux->p_sys;
+	demux_Control(p_sys->fdemux, DEMUX_GET_TIME, &cur_time);
+	demux_Control(p_sys->fdemux, DEMUX_GET_PTS_DELAY, &pts_delay);
+	time_interval *cur_scene = &p_sys->scenes[p_sys->cur_scene];
 	
-	if (p_demux->p_sys->cur_scene == p_demux->p_sys->scenes_num - 1 && cur_time >= cur_scene->end)
+	if (p_sys->cur_scene == p_sys->scenes_num - 1 && cur_time >= cur_scene->end)
 	{
 		msg_Dbg( p_demux, "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~NTFF DONE");
 		return VLC_DEMUXER_EOF;
@@ -137,33 +215,51 @@ static int Demux( demux_t * p_demux )
 	static int seek_num = 0;
 	static int calls = 0;
 	
-	if (cur_time >= cur_scene->end)
+	if (0)
+	//if (cur_time >= cur_scene->end)
 	{
-		p_demux->p_sys->cur_scene++;
-		cur_scene = &p_demux->p_sys->scenes[p_demux->p_sys->cur_scene];
+		mtime_t pi_system, pi_delay;
+		mtime_t ppi_system, ppi_delay;
+		
+		es_out_ControlGetPcrSystem(p_demux->out, &ppi_system, &ppi_delay);
+		//es_out_Control(p_demux->out, ES_OUT_MODIFY_PCR_SYSTEM, true, ppi_system + 6031513 /*cur_scene->end - cur_scene->begin*/);
+		//es_out_Control(p_demux->out, ES_OUT_SET_PCR, ppi_system + cur_scene->end - cur_scene->begin);
+		es_out_ControlGetPcrSystem(p_demux->out, &pi_system, &pi_delay);
+		
+		msg_Dbg( p_demux, "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~NTFF PCR sys = %li - > %li, scene len = %li", 
+			ppi_system, pi_system, cur_scene->end - cur_scene->begin);
+		
+		p_sys->cur_scene++;
+		cur_scene = &p_sys->scenes[p_sys->cur_scene];
 		
 		msg_Dbg( p_demux, "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~NTFF SEEK TO START of %i (%li -> %li)", 
-			p_demux->p_sys->cur_scene, cur_time, cur_scene->begin);
+			p_sys->cur_scene, cur_time, cur_scene->begin);
 		
-		es_out_Control(p_demux->out, ES_OUT_RESET_PCR);
-		es_out_Control(p_demux->out, ES_OUT_SET_NEXT_DISPLAY_TIME, cur_scene->begin);
-		demux_Control(p_demux->p_sys->fdemux, DEMUX_SET_TIME, cur_scene->begin, true);
+		//es_out_Control(p_demux->out, ES_OUT_RESET_PCR);
+		demux_Control(p_sys->fdemux, DEMUX_SET_TIME, cur_scene->begin, true);
+		//es_out_Control(p_demux->out, ES_OUT_SET_NEXT_DISPLAY_TIME, cur_scene->begin);
+		//msg_Dbg( p_demux, "~~~~~~~ES_OUT_MODIFY_PCR_SYSTEM res = %i", res);
+		//es_out_ControlModifyPcrSystem( es_out_t *out, bool b_absolute, mtime_t i_system )
+		//es_out_Control(p_demux->out, ES_OUT_SET_PCR, cur_scene->begin);
+		//mtime_t time1, time2;
+		//es_out_Control(p_demux->out, ES_OUT_GET_PCR_SYSTEM, &time1, &time2);
+		//msg_Dbg( p_demux, "~~~~~~~~~~~ES_OUT_GET_PCR_SYSTEM = %li, %li", time1, time2);
 	}
 	
 	/*if (((cur_time < cur_scene->begin - pts_delay)) && 
 		!seek_num)
 	{
-		int res = demux_Control(p_demux->p_sys->fdemux, DEMUX_SET_TIME, cur_scene->begin, true);
+		int res = demux_Control(p_sys->fdemux, DEMUX_SET_TIME, cur_scene->begin, true);
 		es_out_Control(p_demux->out, ES_OUT_RESET_PCR);
 		msg_Dbg( p_demux, "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~NTFF SEEK TO START of %i (%li -> %li), res = %i", 
-			p_demux->p_sys->cur_scene, cur_time, cur_scene->begin, res);
-		//demux_Control(p_demux->p_sys->fdemux, DEMUX_GET_TIME, &cur_time);
+			p_sys->cur_scene, cur_time, cur_scene->begin, res);
+		//demux_Control(p_sys->fdemux, DEMUX_GET_TIME, &cur_time);
 		//seek_num++;
 	}*/
 	
 	calls++;
-	int demux_res = p_demux->p_sys->fdemux->pf_demux(p_demux->p_sys->fdemux);
-	msg_Dbg( p_demux, "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~NTFF Demux cur_time = %li, res = %i", cur_time, demux_res);
+	int demux_res = p_sys->fdemux->pf_demux(p_sys->fdemux);
+	msg_Dbg( p_demux, "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~NTFF Demux cur_time = %li", cur_time);
 	return demux_res;
 }
 
@@ -175,48 +271,79 @@ int64_t kdenlive_time(int h, int m, int s, int ms)
 static int Open(vlc_object_t *p_this)
 {	
 	demux_t *p_demux = (demux_t *)p_this;
-	p_demux->p_sys = calloc( 1, sizeof( demux_sys_t ) );
+	demux_sys_t *p_sys = calloc( 1, sizeof( demux_sys_t ) );
+	p_demux->p_sys = p_sys;
 	p_demux->pf_demux = Demux;
     p_demux->pf_control = Control;
 	
+	ntff_register_es(p_demux, &p_sys->es);
+	p_sys->need_skip_scene = false;
+	
+//#define COLOR
+//#define EXPANSE
+#ifdef COLOR
+	const char *mrl = "file:///home/elventian/colors-2397.mp4";
+	const char *psz_filepath = "/home/elventian/colors-2397.mp4";
+#else
+#ifdef EXPANSE
 	const char *mrl = "file:///home/elventian/Expanse.mkv";
-	const char *psz_name = "any";
 	const char *psz_filepath = "/home/elventian/Expanse.mkv";
-	p_demux->p_sys->stream = vlc_stream_NewMRL(p_this, mrl);
+#else
+	const char *mrl = "file:///home/elventian/s01e01_Something.Wicca.This.Way.Comes.avi";
+	const char *psz_filepath = "/home/elventian/s01e01_Something.Wicca.This.Way.Comes.avi";
+#endif
+#endif
+	const char *psz_name = "any";
+	p_sys->stream = vlc_stream_NewMRL(p_this, mrl);
 	
 	
-	msg_Dbg( p_demux, "~~~~NTFF Open = %s, %s", p_demux->p_sys->stream->psz_name, 
-		p_demux->p_sys->stream->psz_filepath);
+	msg_Dbg( p_demux, "~~~~NTFF Open = %s, %s", p_sys->stream->psz_name, 
+		p_sys->stream->psz_filepath);
 	
-	p_demux->p_sys->fdemux = demux_New(p_this, psz_name,
-		psz_filepath, p_demux->p_sys->stream, p_demux->out);
+	p_sys->fdemux = demux_New(p_this, psz_name,
+		psz_filepath, p_sys->stream, &p_sys->es);
 	
-	p_demux->p_sys->scenes_num = 2;
-	p_demux->p_sys->cur_scene = 0;
+	double fps;
+	demux_Control(p_sys->fdemux, DEMUX_GET_FPS, &fps);
+	double frame_len = 1000000 / fps;
 	
-	p_demux->p_sys->scenes[0].begin = kdenlive_time(0, 8, 49, 040);
-	p_demux->p_sys->scenes[0].end   = p_demux->p_sys->scenes[0].begin + kdenlive_time(0, 0, 03, 360);
-	p_demux->p_sys->scenes[1].begin = p_demux->p_sys->scenes[0].end   + kdenlive_time(0, 2, 16, 400);
-	p_demux->p_sys->scenes[1].end   = p_demux->p_sys->scenes[1].begin + kdenlive_time(0, 0, 2, 440);
-	//3
-	//p_demux->p_sys->scenes[1].begin = p_demux->p_sys->scenes[1].end + 52280 * 1000;
-	//p_demux->p_sys->scenes[1].end   = p_demux->p_sys->scenes[1].begin + 17040 * 1000;
-	//4
-	//p_demux->p_sys->scenes[1].begin = p_demux->p_sys->scenes[1].end + 1702960 * 1000;
-	//p_demux->p_sys->scenes[1].end   = p_demux->p_sys->scenes[1].begin + 3440 * 1000;
+	msg_Dbg( p_demux, "~~~~FPS = %f", frame_len);
+	//msg_Dbg( p_demux, "~~~~FPS = %f", fps);
 	
-	//manual
-	/*p_demux->p_sys->scenes[1].begin = 1708512689;
-	p_demux->p_sys->scenes[1].end   = p_demux->p_sys->scenes[1].begin + 2000 * 1000;
+	p_sys->scenes_num = 3;
+	p_sys->cur_scene = 0;
+#ifdef COLOR	
+	p_sys->scenes[0].begin = kdenlive_time(0, 0, 5, 5);
+	p_sys->scenes[0].end   = p_sys->scenes[0].begin + kdenlive_time(0, 0, 0, 959) + frame_len;
+	p_sys->scenes[1].begin = p_sys->scenes[0].end   + kdenlive_time(0, 0, 4, 4);
+	p_sys->scenes[1].end   = p_sys->scenes[1].begin + kdenlive_time(0, 0, 0, 959) + frame_len;
+	p_sys->scenes[2].begin = p_sys->scenes[1].end   + kdenlive_time(0, 0, 4, 4);
+	p_sys->scenes[2].end   = p_sys->scenes[2].begin + kdenlive_time(0, 0, 0, 959) + frame_len;
+#else	
+#ifdef EXPANSE
+	p_sys->scenes[0].begin = kdenlive_time(0, 8, 49, 40);
+	p_sys->scenes[0].end   = p_sys->scenes[0].begin + kdenlive_time(0, 0, 3, 360) + frame_len;
+	p_sys->scenes[1].begin = p_sys->scenes[0].end   + kdenlive_time(0, 2, 16, 400);
+	p_sys->scenes[1].end   = p_sys->scenes[1].begin + kdenlive_time(0, 0, 6, 80) + frame_len;
+	p_sys->scenes[2].begin = p_sys->scenes[1].end   + kdenlive_time(0, 28, 33, 920);
+	p_sys->scenes[2].end   = p_sys->scenes[2].begin + kdenlive_time(0, 0, 3, 360) + frame_len;
 	
-	p_demux->p_sys->scenes[2].begin = 402450586;
-	p_demux->p_sys->scenes[2].end   = p_demux->p_sys->scenes[2].begin + 2000 * 1000;*/
+	//p_sys->scenes[1].end   += 19 * frame_len;
+	msg_Dbg( p_demux, "~~~~scene end = %li", p_sys->scenes[1].end);
+#else	
+	p_sys->scenes[0].begin = kdenlive_time(0, 11, 14, 400);
+	p_sys->scenes[0].end   = p_sys->scenes[0].begin + kdenlive_time(0, 0, 2, 880) + frame_len;
+	p_sys->scenes[1].begin = p_sys->scenes[0].end   + kdenlive_time(0, 13, 9, 840);
+	p_sys->scenes[1].end   = p_sys->scenes[1].begin + kdenlive_time(0, 0, 2, 720) + frame_len;
+	p_sys->scenes[2].begin = p_sys->scenes[1].end   + kdenlive_time(0, 15, 25, 0);
+	p_sys->scenes[2].end   = p_sys->scenes[2].begin + kdenlive_time(0, 0, 4, 40) + frame_len;
 	
+#endif	
+#endif	
+	demux_Control(p_sys->fdemux, DEMUX_SET_TIME, p_sys->scenes[0].begin, true);
 	
-	//p_demux->p_sys->scenes[1].begin = 1534623355;
-	//p_demux->p_sys->scenes[1].end   = 1572536279;
-	
-	demux_Control(p_demux->p_sys->fdemux, DEMUX_SET_TIME, p_demux->p_sys->scenes[0].begin, true);
+   /*int dres = vlc_dialog_wait_question(p_this, VLC_DIALOG_QUESTION_NORMAL, "cancel", "psz_action1",
+   "psz_action2", "psz_title", "");*/
 	
 	return VLC_SUCCESS;
 	//return VLC_EGENERIC;
